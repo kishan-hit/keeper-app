8e7f8d2d103e53725e25ece8589d7a492bc4aa54
Kishan Kumar <kishan.kumar@zimtive.com>
2023-01-26 17:48:25 +0530

    updated

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,12 +2,32 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
+import notes from "../notes";
+
+const createNotes = (note) => {
+    return(
+        <Note 
+            key={note.key}
+            title={note.title}
+            content={note.content}
+        />
+    );
+}
 
 const App = () => {
     return(
         <>
             <Header />
-            <Note />
+            {notes.map(createNotes)}
+
+            {/* {notes.map((note) => 
+                <Note 
+                    key = {note.key}
+                    title = {note.title}
+                    content = {note.content}
+                />
+            )} */}
+
             <Footer />
         </>
     );
